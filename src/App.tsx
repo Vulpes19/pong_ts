f2400e2bcfeeee084cc7947ecb41a602ecbfec3e
Vulpes19 <baioumyaymann@gmail.com>
2023-10-14 17:10:27 +0100

    simple movement for paddle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import { useState } from 'react'
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let y: number = 250;
+  useEffect( () => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D | null;
+    ctx = canvas.getContext("2d");
+    const paddle1 = new Image();
+    const paddle2 = new Image();
+    const ball = new Image();
+    paddle1.src = "src/assets/paddle.png";
+    paddle2.src = "src/assets/paddle.png";
+    ball.src = "src/assets/ball.png";
+    function draw() {
+      if (canvas)
+      {
+        ctx?.clearRect(0, 0, canvas.width, canvas.height);
+        if (ctx)
+          ctx.fillStyle = "black";
+        //background
+        ctx?.fillRect(0, 0, 800, 600);
+        //paddle1
+        ctx?.drawImage(paddle1, 0, y);
+        //paddle2
+        ctx?.drawImage(paddle1, 780, 250);
+        //ball
+        ctx?.drawImage(ball, 400, 300);
+      }
+    };
+    draw();
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key == 'ArrowUp')
+      {
+        console.log('arrow up');
+        y -= 10;
+      }
+      if (event.key == 'ArrowDown')
+      {
+        console.log('arrow down');
+        y += 10;
+      }
+      draw();
+    } 
+  }, [y]);
 
   return (
     <>
-      <div>pskch</div>
+      <canvas id='canvas' width={800} height={600}></canvas>
     </>
   )
 }
