ce15f36dc7d38a7e4ccc4091de3144ca7cba5dbc
Vulpes19 <baioumyaymann@gmail.com>
2023-10-20 13:59:34 +0100

    the countdown is implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,79 +2,86 @@ import { KeyboardEventHandler, useEffect, useRef } from "react";
 import { useState } from "react";
 import { Stage, Layer, Image, Text, Rect } from "react-konva";
 import Konva from "konva";
-interface Vertices {
+interface Vector {
 	x: number,
 	y: number
 };
 
-interface Ball {
-	position: Vertices,
-	velocity: Vertices,
-	img: HTMLImageElement
+interface Textures {
+	ballTexture: HTMLImageElement,
+	paddleTexture: HTMLImageElement 
 };
-
-interface Paddle {
-	position: Vertices,
-	img: HTMLImageElement
-};
-
-function addVertices(v1: Vertices, v2: Vertices): Vertices {
-	// console.log(v1.x, v2.x)
-	let ret: Vertices = {
-		x: v1.x + v2.x,
-		y: v1.y + v2.y
-	};
-	return (ret);
-};
-
 
 const WIDTH: number = 800;
 const HEIGHT: number = 600;
 
 function App() {
-	const player: Paddle = {
-		position: {x: 0, y: 250},
-		img: new window.Image()
-	};
-	const oponent: Paddle = {
-		position: {x: 780, y: 250},
-		img: new window.Image()
-	};
-	const ball: Ball = {
-		position: {x: 400, y: 300},
-		velocity: {x: 0, y: 10},
-		img: new window.Image()
-	};
-	const [position, setPositon] = useState<Vertices>({x:0, y: 250});
+	const textures: Textures = {
+		ballTexture: new window.Image,
+		paddleTexture: new window.Image,
+	}
+	const [playerPosition, setPlayerPositon] = useState<Vector>({x:0, y: 250});
+	const [oponentPosition, setOponentrPositon] = useState<Vector>({x:780, y: 250});
 	const [ballX, setBallX] = useState<number>(400);
 	const [ballY, setBallY] = useState<number>(300);
 	const [velX, setVelX] = useState<number>(5);
 	const [velY, setVelY] = useState<number>(5);
-	const [isRunning, setRunning] = useState<boolean>(false);
+	const [isRunning, setRunning] = useState<boolean>(true);
 	const [playerScore, setPlayerScore] = useState<number>(0);
 	const [oponentScore, setOponentScore] = useState<number>(0);
-	player.img.src = 'assets/paddle.png';
-	oponent.img.src = 'assets/paddle.png';
-	ball.img.src = 'assets/ball.png';
+	const [count, setCount] = useState<number>(3);
+	textures.paddleTexture.src = 'assets/paddle.png';
+	textures.ballTexture.src = 'assets/ball.png';
 	const handleMovement = (e: KeyboardEvent) => {
-		if (e.key === 'ArrowUp' && position.y > 0 )
+		if (e.key === 'ArrowUp' && playerPosition.y > 0 )
 		{
-			setPositon({x: 0, y: position.y - 20})
+			const pos: Vector = {x: 0, y: playerPosition.y - 20};
+			setPlayerPositon(pos);
+			// setPlayerPositon({x: 0, y: playerPosition.y - 20})
 		}
-		if (e.key === 'ArrowDown' && position.y + 100 < 600)
+		if (e.key === 'ArrowDown' && playerPosition.y + 100 < 600)
 		{
-			setPositon({x: 0, y: position.y + 20})
+			const pos: Vector = {x: 0, y: playerPosition.y + 20};
+			setPlayerPositon(pos);
+			// setPlayerPositon({x: 0, y: playerPosition.y + 20})
 		}
 	}
 	useEffect( () => {
+		if (isRunning === false)
+		{
+			if (count === 0)
+			{
+				setRunning(true);
+				setCount(3);
+				return ;
+			}
+			console.log('helo')
+			const id: number = setInterval( () => {
+				setCount(count - 1);
+			}, 1000);
+			return () => {
+				clearInterval(id);
+			}
+		}
+	}, [isRunning, count])
+
+	useEffect( () => {
 		const updateBall = () => {
+			//collision with side window borders
 			setBallX( (x: number): number => {
-				if (x < position.x + 25 && ballY >= position.y && ballY <= position.y + 100)
+				//player paddle collision
+				if (x < playerPosition.x + 25 && ballY >= playerPosition.y && ballY <= playerPosition.y + 100)
 					setVelX(5);
-				if (x > oponent.position.x && ballY >= oponent.position.y && ballY <= position.y + 100)
+				//oponent paddle collision
+				if (x > oponentPosition.x && ballY >= oponentPosition.y && ballY <= playerPosition.y + 100)
 					setVelX(-5);
+				//oponent scores
 				if (x < 0)
 				{
+					//resets player paddle position
+					setRunning(false);
+					const pos: Vector = {x: 0, y: 250};
+					setPlayerPositon(pos);
 					setOponentScore(oponentScore + 1);
 					if (Math.floor(Math.random() * 4) > 2)
 						setVelX(-5);
@@ -83,6 +90,7 @@ function App() {
 					setBallY(300);
 					return (400);
 				}
+				//player scores
 				if (x + 40 > WIDTH)
 				{
 					setPlayerScore(playerScore + 1);
@@ -95,14 +103,16 @@ function App() {
 				}
 				return (x + velX);
 			})
+			//collision with the top and bottom window border
 			setBallY( (y: number): number => {
-				if (y < 0 )
+				if (y <= 0 )
 					setVelY(5);
-				if (y + 40 > HEIGHT)
+				if (y + 30 >= HEIGHT)
 					setVelY(-5);
 				return (y + velY);
 			})
 		}
+		//player movement
 		window.addEventListener('keydown', handleMovement);
 		let id: number = requestAnimationFrame(updateBall);
 		return () => {
@@ -115,14 +125,16 @@ function App() {
 			<Layer>
 				<Rect width={WIDTH} height={HEIGHT} fill="black"></Rect>
 			</Layer>
+			{isRunning ? (
 			<Layer>
 				<Text text={playerScore.toString() + ' : ' + oponentScore.toString()} x={360} y={20} fill="white" fontSize={50}></Text>
-			</Layer>
-			<Layer>
-				<Image image={player.img} x={position.x} y={position.y} />
-				<Image image={oponent.img} x={oponent.position.x} y={oponent.position.y} />
-				<Image image={ball.img} x={ballX} y={ballY} />
-			</Layer>
+				<Image image={textures.paddleTexture} x={playerPosition.x} y={playerPosition.y} />
+				<Image image={textures.paddleTexture} x={oponentPosition.x} y={oponentPosition.y} />
+				<Image image={textures.ballTexture} x={ballX} y={ballY} />
+			</Layer>) : 
+			(<Layer>
+				<Text text={'wait ' + count.toString()} fill="white" x={350} y={300} fontSize={40}></Text>
+			</Layer>) } 
 		</Stage>
 		);
 	}
